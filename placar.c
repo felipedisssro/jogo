29dc36b41e31bed39652295fa9018882d6e06901
felipedisssro <felipe.cdissero@gmail.com>
2025-08-18 22:57:25 -0300

    Add files via upload

---
+++ placar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+   
+int golsc; //corinthians
+int golsp; // palmeiras
+
+printf ("digite quantos gols o Corinthians marcou\n");
+scanf ("%d", &golsc);
+
+printf ("digite quantos gols o palmeiras marcou\n");
+scanf ("%d", &golsp);
+
+if (golsc > golsp){
+   printf ("corinthians ganhou\n");
+} else if (golsp > golsc){
+   printf ("palmeiras ganhou\n");
+} else 
+   printf ("empate\n");
+} 
+
+    return 0;
+}
+
+
+
+
+
+
+
+
